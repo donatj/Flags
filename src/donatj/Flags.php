0584379d43ec6f0a6afa9f9ab262ad8f093a39cf
Jesse Donat <donatj@gmail.com>
2013-11-19 16:04:57 -0600

    Lots of documentation added

--- src/donatj/Flags.php
+++ src/donatj/Flags.php
@@ -12,17 +12,30 @@ class Flags {
 	private $defined_short_flags = array();
 	private $arguments = array();
 
+	/**
+	 * Returns the n'th command-line argument. `arg(0)` is the first remaining argument after flags have been processed.
+	 *
+	 * @param int $index
+	 * @return string
+	 */
 	public function arg( $index ) {
 		return isset($this->arguments[$index]) ? $this->arguments[$index] : null;
 	}
 
 	/**
-	 * @return string[]
+	 * Returns the non-flag command-line arguments.
+	 *
+	 * @return string[] Array of argument strings
 	 */
 	public function args() {
 		return $this->arguments;
 	}
 
+	/**
+	 * Returns an array of long-flag values indexed by flag name
+	 *
+	 * @return array
+	 */
 	public function longs() {
 		$out = array();
 		foreach( $this->defined_flags as $key => $data ) {
@@ -32,6 +45,13 @@ class Flags {
 		return $out;
 	}
 
+	/**
+	 * Returns an array of short-flag call-counts indexed by character
+	 *
+	 * `-v` would set the 'v' index to 1, whereas `-vvv` will set the 'v' index to 3
+	 *
+	 * @return array
+	 */
 	public function shorts() {
 		$out = array();
 		foreach( $this->defined_short_flags as $key => $data ) {
@@ -41,6 +61,19 @@ class Flags {
 		return $out;
 	}
 
+	/**
+	 * Defines a short-flag of specified name, and usage string.
+	 * The return value is a reference to an integer variable that stores the number of times the short-flag was called.
+	 *
+	 * This means the value of the reference for v would be the following.
+	 *
+	 *     -v => 1
+	 *     -vvv => 3
+	 *
+	 * @param string $letter The character of the short-flag to define
+	 * @param string $usage The usage description
+	 * @return int
+	 */
 	public function &short( $letter, $usage = '' ) {
 		$this->defined_short_flags[$letter[0]] = array(
 			'value' => 0,
@@ -50,26 +83,119 @@ class Flags {
 		return $this->defined_short_flags[$letter[0]]['value'];
 	}
 
+	/**
+	 * Defines a bool long-flag of specified name, default value, and usage string.
+	 * The return value is a reference to a variable that stores the value of the flag.
+	 *
+	 * Examples:
+	 *
+	 * Truth-y:
+	 *
+	 *      --mybool=[true|t|1]
+	 *      --mybool [true|t|1]
+	 *      --mybool
+	 *
+	 * False-y:
+	 *
+	 *      --mybool=[false|f|0]
+	 *      --mybool [false|f|0]
+	 *        [not calling --mybool and having the default false]
+	 *
+	 * @param string $name The name of the long-flag to define.
+	 * @param mixed  $value The default value - usually false.
+	 * @param string $usage The usage description
+	 * @return mixed A reference to the flags value
+	 */
 	public function &bool( $name, $value = null, $usage = '' ) {
 		return $this->_storeFlag('bool', $name, $value, $usage);
 	}
 
+	/**
+	 * Defines a float long-flag of specified name, default value, and usage string.
+	 * The return value is a reference to a variable that stores the value of the flag.
+	 *
+	 * Examples:
+	 *
+	 *     --myfloat=1.1
+	 *     --myfloat 1.1
+	 *
+	 * @param string $name The name of the long-flag to define.
+	 * @param mixed  $value The default value
+	 * @param string $usage The usage description
+	 * @return mixed A reference to the flags value
+	 */
 	public function &float( $name, $value = null, $usage = '' ) {
 		return $this->_storeFlag('float', $name, $value, $usage);
 	}
 
+	/**
+	 * Defines an integer long-flag of specified name, default value, and usage string.
+	 * The return value is a reference to a variable that stores the value of the flag.
+	 *
+	 * Note: Float values trigger an error, rather than casting.
+	 *
+	 * Examples:
+	 *
+	 *     --myinteger=1
+	 *     --myinteger 1
+	 *
+	 * @param string $name The name of the long-flag to define.
+	 * @param mixed  $value The default value
+	 * @param string $usage The usage description
+	 * @return mixed A reference to the flags value
+	 */
 	public function &int( $name, $value = null, $usage = '' ) {
 		return $this->_storeFlag('int', $name, $value, $usage);
 	}
 
+	/**
+	 * Defines a unsigned integer long-flag of specified name, default value, and usage string.
+	 * The return value is a reference to a variable that stores the value of the flag.
+	 *
+	 * Note: Negative values trigger an error, rather than casting.
+	 *
+	 * Examples:
+	 *
+	 *     --myinteger=1
+	 *     --myinteger 1
+	 *
+	 * @param string $name The name of the long-flag to define.
+	 * @param mixed  $value The default value
+	 * @param string $usage The usage description
+	 * @return mixed A reference to the flags value
+	 */
 	public function &uint( $name, $value = null, $usage = '' ) {
 		return $this->_storeFlag('uint', $name, $value, $usage);
 	}
 
+	/**
+	 * Defines a string long-flag of specified name, default value, and usage string.
+	 * The return value is a reference to a variable that stores the value of the flag.
+	 *
+	 * Examples
+	 *
+	 *     --mystring=vermouth
+	 *     --mystring="blind jazz singers"
+	 *     --mystring vermouth
+	 *     --mystring "blind jazz singers"
+	 *
+	 * @param string $name The name of the long-flag to define.
+	 * @param mixed  $value The default value
+	 * @param string $usage The usage description
+	 * @return mixed A reference to the flags value
+	 */
 	public function &string( $name, $value = null, $usage = '' ) {
 		return $this->_storeFlag('string', $name, $value, $usage);
 	}
 
+
+	/**
+	 * @param string $type
+	 * @param string $name
+	 * @param mixed  $value
+	 * @param string $usage
+	 * @return mixed
+	 */
 	private function &_storeFlag( $type, $name, $value, $usage ) {
 
 		$this->defined_flags[$name] = array(
@@ -83,6 +209,19 @@ class Flags {
 
 	}
 
+	/**
+	 * Returns the default values of all defined command-line flags as a formatted string.
+	 *
+	 * Example:
+	 *
+	 *               -v   Output in verbose mode
+	 *      --testsuite   [string] Which test suite to run.
+	 *      --bootstrap   [string] A "bootstrap" PHP file that is run before the specs.
+	 *           --help   Display this help message.
+	 *        --version   Display this applications version.
+	 *
+	 * @return string
+	 */
 	public function getDefaults() {
 
 		$output = '';
@@ -113,7 +252,12 @@ class Flags {
 	}
 
 	/**
-	 * @param array $args
+	 * Parses flag definitions from the argument list, which should not include the command name.
+	 * Must be called after all flags are defined and before flags are accessed by the program.
+	 *
+	 * Will throw exceptions on Missing Require Flags, Unknown Flags or Incorrect Flag Types
+	 *
+	 * @param array $args The arguments to parse, defaults to $GLOBALS['argv']
 	 * @param bool  $ignoreExceptions
 	 * @throws Exceptions\MissingFlagParamException
 	 * @throws Exceptions\InvalidFlagParamException

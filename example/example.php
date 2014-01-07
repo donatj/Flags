184577846113fea76870df26777d79b8ff65beff
Jesse Donat <j.donat@capstonedigital.com>
2014-01-07 16:42:50 -0600

    Example cleaned up a bit

--- example/example.php
+++ example/example.php
@@ -1,16 +1,21 @@
 <?php
 
-require('../vendor/autoload.php');
+require(__DIR__ . '/../vendor/autoload.php');
 
 $flags = new donatj\Flags();
 
-$sponges = & $flags->bool('foo', false, 'Enable the foo');
-$what    = & $flags->uint('bar', 10, 'Number of bars');
-$cat     = & $flags->string('baz', 'Fred', 'What to name the baz');
+$foo     = & $flags->bool('foo', false, 'Enable the foo');
+$bar     = & $flags->uint('bar', 10, 'Number of bars');
+$baz     = & $flags->string('baz', 'Fred', 'What to name the baz');
 $verbose = & $flags->short('v', 'verbosity');
+
+/**
+ * No Default value, making qux is *required*
+ */
+$qux = & $flags->bool('qux');
 
 try {
 	$flags->parse();
 } catch(Exception $e) {
-	die($e->getMessage() . PHP_EOL . $flags->getDefaults() . PHP_EOL );
+	die($e->getMessage() . PHP_EOL . $flags->getDefaults() . PHP_EOL);
 }
